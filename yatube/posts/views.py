b1d493cbc63d6f19ef5cea2b23b83cc5de6c08de
Серей Овинцев <ozonti@mail.ru>
2023-03-09 19:44:03 +0300

    add folder templates, create html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    context = {
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
 
 
 def group_posts(request):
-    return HttpResponse('Список микро-блогеров и их постов')
+    template = 'posts/group_list.html'
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
